ebed483b8f9df2cedf677025677aebc23a86380f
Rahul <rahulrajesh@2101.gmail.com>
2024-10-03 14:16:35 +0530

    vercel

--- config/database.js
+++ config/database.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 exports.connect = async () => {
-try{
-    mongoose.connect(process.env.MONGO_URL).then(()=>{
-        console.log('Database connected');
-    })
-}catch(err){
-    console.error(`Error connecting to the database. \n${err}`);
-}
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+    
+    });
+    console.log('Database connected');
+  } catch (err) {
+    console.error(`Error connecting to the database: ${err.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
